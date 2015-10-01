4b880154e06687591aea33d0518a3a9f6e8e220d
Carlos <cbarranco@decktra.com>
2015-10-01 16:53:16 -0430

    Juan´s Web Client and Back Button

---
+++ MainActivity.java
@@ -0,0 +1,91 @@
+﻿package com.example.elcarlos.test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView myWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        myWebView = (WebView) findViewById(R.id.webView); 
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        myWebView.loadUrl("http://72.9.150.72/~decktrac/ecooking.com/");
+
+	//DUDA #1: no se si para agregar el nuevo cliente (linea 23) tengo que eliminar esta linea o dejarla
+	// R: debes quitarla
+        //myWebView.setWebViewClient(new WebViewClient()); 
+        //DUDA #1: Añadir un webclient personalizado
+        
+	myWebView.setWebViewClient(new MyWebViewClient());
+
+    }
+
+    //DUDA #2: Sobreescribir el comportamiento default del boton "atras" (en el hardware del telefono) para que al presionarlo no se salga de la app, sino que vaya a la pagina anterior
+    //creo que para que el paso de la DUDA #2 funcione, tiene que hacerse el de la DUDA #1, para que se cree una historia, pero no estoy seguro jajaja
+    // R: Si vas a usar la opcion 2, cambia la linea: 
+    // WebView myWebView = (WebView) findViewById(R.id.webView); del metodo onCreate que tienes arriba, por
+    // myWebView = (WebView) findViewById(R.id.webView); 
+    // creo que el problema es que tienes dos variables diferentes
+    
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // Check if the key event was the Back button and if there's history
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && myWebView.canGoBack()) {
+            myWebView.goBack();
+            return true;
+        }
+        // If it wasn't the Back key or there's no web page history, bubble up to the default
+        // system behavior (probably exit the activity)
+        return super.onKeyDown(keyCode, event);
+    }*/
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
+
+//DUDA #1: Añadir un webclient personalizado
+// Definela fuera de la clase principal
+class MyWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            if (Uri.parse(url).getHost().equals("www.example.com")) {
+                // This is my web site, so do not override; let my WebView load the page
+                return false;
+            }
+            // Otherwise, the link is not for a page on my site, so launch another Activity that handles URLs
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            startActivity(intent);
+            return true;
+        }
+    }
+
